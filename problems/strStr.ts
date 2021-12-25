c752392cae3b3369425f715cd3cb91f77a136da6
drewosmond <drewosmond@gmail.com>
2021-12-24 17:22:51 -0800

    updated comments to strStr and twoSum

--- problems/strStr.ts
+++ problems/strStr.ts
@@ -1,19 +1,23 @@
 function strStr(haystack: string, needle: string): number {
+  //if both values are empty, return 0 since the empty string is at the 0th index.
   if (haystack.length === 0 && needle.length === 0) {
     return 0;
   }
-
+  //loop through the haystack
   for (let i = 0; i < haystack.length; i++) {
     let isNeedle: boolean = true;
+    //loop through the length of the needle and see if the substring matches the needle
     for (let j = 0; j < needle.length; j++) {
       const stringIdx: string = haystack[i + j];
       const needleIdx: string = needle[j];
+      //if there is a value of the substring that does not match, then it's not the index
       if (stringIdx !== needleIdx) {
         isNeedle = false;
       }
     }
 
     if (isNeedle) {
+      //return early if we find the index of the substring (needle)
       return i;
     }
   }
--- problems/twoSum.ts
+++ problems/twoSum.ts
@@ -1,7 +1,11 @@
 function twoSum(nums: number[], target: number): number[] {
+  //a map of all the keys which correspond with a unique number in nums.
+  //the value of each number is an array of touples which stores the idx of each number
+  //as well as a boolean on if that index has been added to the returned array or not.
   const mappedNums: { [key: string]: [number, boolean][] } = {};
   const indexesToReturn: number[] = [];
 
+  //map each number and add it into our mappedNums
   nums.forEach((num, i) => {
     if (mappedNums[num]) {
       mappedNums[num].push([i, false]);
@@ -10,12 +14,19 @@ function twoSum(nums: number[], target: number): number[] {
     }
   });
 
+  //loop through the nums array to look at each number
   for (let i = 0; i < nums.length; i++) {
+    //calculate the difference between the current selected number and the target
     const difference = target - nums[i];
+    //check to see if the difference is a number in our mapped array
     if (mappedNums[difference]) {
+      //loop through the array of touples in our mapped array
       for (let j = 0; j < mappedNums[difference].length; j++) {
         const touple = mappedNums[difference][j];
+        //looks to see if that number has a unique index that has not been used yet
+        //and that the idx is not the current number in edge cases where that number plus itself === target
         if (!touple[1] && touple[0] !== i) {
+          //mark idx as used and push the index
           touple[1] = true;
           indexesToReturn.push(i);
           break;
