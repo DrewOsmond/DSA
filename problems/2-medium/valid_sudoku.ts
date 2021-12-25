907de01a13b2336824ffaff20dce24e22d4d91b5
drewosmond <drewosmond@gmail.com>
2021-12-24 17:41:53 -0800

    added node modules for Set type def

--- problems/2-medium/valid_sudoku.ts
+++ problems/2-medium/valid_sudoku.ts
@@ -1,6 +1,39 @@
 //check and see if a sudoku is a valid sudoku
 
-function isValidSudoku(board: string[][]): boolean {}
+/* 
+plan:
+    loop through the rows and collumns to make sure those are valid,
+    then break the board into the 3x3 board 
+    check to make sure each inner board is also correct
+*/
+
+function isValidSudoku(board: string[][]): boolean {
+  for (let row of board) {
+    const validRow = checkRow(row);
+    if (!validRow) {
+      return false;
+    }
+  }
+
+  for (let i = 0; i < 9; i++) {
+    const validColumn = checkColumn(board, i);
+    if (!validColumn) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function checkRow(row: string[]): boolean {
+  const checkedNumbers = new Set();
+  const set = new Set();
+  //loop through each row and make sure it's valid
+}
+
+function checkColumn(board: string[][], columnIdx: number): boolean {
+  //loop through each column and make sure it's valid
+}
 
 //example
 // Input: board =
