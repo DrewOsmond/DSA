4ca69504a415cb48dee4326bf51bc4b2345c2a25
Demistal <73513615+Demistal@users.noreply.github.com>
2021-12-28 00:14:17 -0800

    finished best time to buy and sell

--- leet_code/1-easy/best_time_to_buy_and_sell_stock/index.ts
+++ leet_code/1-easy/best_time_to_buy_and_sell_stock/index.ts
@@ -1,3 +1,14 @@
 function maxProfit(prices: number[]): number {
-  return 5;
+  let highestProfit: number = -Infinity;
+  let cheapestBuy: number = prices[0];
+
+  for (let i = 1; i < prices.length; i++) {
+    const buy: number = prices[i];
+    highestProfit = Math.max(highestProfit, buy - cheapestBuy);
+    cheapestBuy = Math.min(cheapestBuy, buy);
+  }
+
+  return highestProfit;
 }
+
+console.log(maxProfit([7, 6, 4, 3, 1]));
--- leet_code/1-easy/intersection_of_two_arrays_II/index.ts
+++ leet_code/1-easy/intersection_of_two_arrays_II/index.ts
@@ -2,13 +2,18 @@ interface MappedNums {
   [key: number]: number;
 }
 
+//time complexity: O(n)
 function intersect(nums1: number[], nums2: number[]): number[] {
+  //create a map of nums2 values O(n) time so we can directly
+  //check nums2 vals in O(1) time.
   const nums2Values: MappedNums = addNumsToMap(nums2);
   const intersectedArray: number[] = [];
 
   for (let num of nums1) {
     const val: number | undefined = nums2Values[num];
+    //if this number is a key in the map, and if the number is greater than 0;
     if (val) {
+      //if there is a value, subtract one from the mapped numbers as well as push the number into the intersected array
       intersectedArray.push(num);
       nums2Values[num]--;
     }
