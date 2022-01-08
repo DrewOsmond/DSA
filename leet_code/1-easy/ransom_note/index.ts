4aaef640ec0ea933d9276a5e8ff952cfcbfe4766
drewosmond <drewosmond@gmail.com>
2022-01-07 19:08:32 -0800

    finished ransom_note

---
+++ leet_code/1-easy/ransom_note/index.ts
@@ -0,0 +1,21 @@
+//big O = O(n)
+
+function canConstruct(ransomNote: string, magazine: string): boolean {
+  const magazineCount: { [key: string]: number } = {};
+
+  for (let char of magazine) {
+    magazineCount[char]
+      ? (magazineCount[char] = magazineCount[char] + 1)
+      : (magazineCount[char] = 1);
+  }
+
+  for (let char of ransomNote) {
+    if (magazineCount[char] && magazineCount[char] > 0) {
+      magazineCount[char] = magazineCount[char] - 1;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
