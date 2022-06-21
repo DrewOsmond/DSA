52d88e59d1e4aeabc997985c004087fd38b86316
drewosmond <drewosmond@gmail.com>
2022-06-20 21:30:37 -0700

    finished deleteNode

---
+++ leet_code/1-easy/delete_node_in_linked_list/index.ts
@@ -0,0 +1,25 @@
+import { ListNode } from "../../abstract_data_types";
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify it in-place instead.
+ */
+function deleteNode(root: ListNode | null): void {
+  if (root && root.next) {
+    root.val = root.next.val;
+    root.next = root.next.next;
+  }
+}
+
+console.log(deleteNode);
