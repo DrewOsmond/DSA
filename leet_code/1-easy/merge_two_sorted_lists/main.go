edd3e03c1f395986d6ef5e4fc2a8dab7bb41248a
drewosmond <drewosmond@gmail.com>
2022-07-29 17:27:20 -0700

    redid merge two sorted lists in go

--- leet_code/1-easy/merge_two_sorted_lists/main.go
+++ leet_code/1-easy/merge_two_sorted_lists/main.go
@@ -1,55 +1,47 @@
 package mergeTwoLists
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+type ListNode struct {
+  Val int
+  Next *ListNode
+}
 
- type ListNode struct {
-	 Val int
-	 Next *ListNode
- }
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+  var newNode *ListNode
 
- func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-    var sortedNode *ListNode
-
-	if list1 == nil {
+    if list1 == nil {
         return list2
-    } else if list2 == nil {
+    } else if list2 == nil { 
         return list1
     }
+    
+  if list1.Val < list2.Val {
+    newNode = list1
+    list1 = list1.Next
+  } else {
+    newNode = list2
+    list2 = list2.Next
+  }
 
-	if list1.Val < list2.Val {
-		sortedNode = list1
-		list1 = list1.Next
-	} else {
-		sortedNode = list2
-		list2 = list2.Next
-	}
+  curr := newNode
 
-	cur := sortedNode
+  for list1 != nil && list2 != nil {
+    if list1.Val <= list2.Val {
+      curr.Next = list1
+      curr = list1
+      list1 = list1.Next
+    } else {
+      curr.Next = list2
+      curr = list2
+      list2 = list2.Next
+    }
+  }
 
-	for list1 != nil && list2 != nil {
-		if list1.Val < list2.Val {
-			cur.Next = list1
-			cur = cur.Next
-			list1 = list1.Next
-		} else {
-			cur.Next = list2
-			cur = cur.Next
-			list2 = list2.Next
-		}
-	}
+  if list1 != nil {
+    curr.Next = list1
+  } else if list2 != nil {
+    curr.Next = list2
+  }
 
-	if list1 != nil {
-		cur.Next = list1
-	} else {
-		cur.Next = list2
-	}
-
-
-	return sortedNode
+  return newNode
 }
+
