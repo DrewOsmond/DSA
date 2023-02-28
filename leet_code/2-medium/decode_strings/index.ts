6c8ad3332f46567c8188cdbc635e23af93de0aa9
drewosmond <drewosmond@gmail.com>
2023-02-27 21:30:01 -0800

    in progress

---
+++ leet_code/2-medium/decode_strings/index.ts
@@ -0,0 +1,14 @@
+// Input: s = "3[a]2[bc]"
+// Output: "aaabcbc"
+
+function decodeString(s: string): string {
+  let stringDecoded = "";
+  let i = 0;
+  let j = 0;
+  let k = 0;
+
+  while (i < s.length) {
+    const charAti = s[i];
+  }
+  return stringDecoded;
+}
